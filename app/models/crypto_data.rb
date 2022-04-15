7b9f4fdce45de296888a0768942bc476b0431583
nadavschwartz <nadavschwartz58@gmail.com>
2022-04-14 18:51:04 -0700

    initialize project with Typescript

--- app/models/crypto_data.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class CryptoData < ApplicationRecord
-end
--- db/migrate/20211003184501_create_crypto_data.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CreateCryptoData < ActiveRecord::Migration[6.0]
-  def change
-    create_table :crypto_data, &:timestamps
-  end
-end
