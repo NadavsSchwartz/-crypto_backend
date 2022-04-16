e379f0d88a8c845dce2b9a004612104534ec1ebf
nadavschwartz <nadavschwartz58@gmail.com>
2022-04-16 09:07:00 -0700

    verifyUser service for user

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,4 +1,22 @@
-import UserModel, { User } from "../model/user.model"
+import UserModel, { User } from '../model/user.model';
+
 export const createUser = async (input: Partial<User>) => {
-return UserModel.create(input)
-}
+  return UserModel.create(input);
+};
+
+export const verifyUser = async (verificationCode: string) => {
+  const foundUser = await UserModel.findOne({
+    verificationCode: verificationCode,
+  });
+
+  if (foundUser) {
+    foundUser.isVerified = true;
+    await foundUser.save();
+    return foundUser;
+  }
+  return null;
+};
+
+export const findUserByEmail = async (email: string) => {
+  return UserModel.findOne({ email });
+};
