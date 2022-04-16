81ded82584ec6909f840d397a7f30a7b83f4056d
nadavschwartz <nadavschwartz58@gmail.com>
2022-04-16 09:04:50 -0700

    validating request resource middlware

--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -1,17 +1,15 @@
-import { Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express';
 
-import {AnyZodObject} from 'zod';
+import { AnyZodObject } from 'zod';
 
 const validateResource = (schema: AnyZodObject) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        try {
-         schema.parse({body: req.body, query: req.query, params: req.params});
-         next();
-        } catch (error: any) {
-            return res.status(400).send(error.errors);
-        }
-       
-        next();
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({ body: req.body, query: req.query, params: req.params });
+      return next();
+    } catch (error: any) {
+      return res.status(400).send(error.errors);
     }
-}
+  };
+};
 export default validateResource;
